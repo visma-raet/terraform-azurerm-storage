b7b0e8b89ec32795ef47df6649bb96f58d6b72ae
mark.stoel@youforce.net <mark.stoel@youforce.net>
2022-02-04 15:11:50 +0100

    container access type should be private

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "azurerm_storage_container" "container" {
   count                 = length(var.containers_list)
   name                  = var.containers_list[count.index].name
   storage_account_name  = azurerm_storage_account.storeacc.name
-  container_access_type = var.containers_list[count.index].access_type
+  container_access_type = "private"
 }
 
 #-------------------------------
